aabcc23e91a3817b14226bfce77250417bd53c92
soo1e <gameswuon@gmail.com>
2024-11-04 01:37:39 +0900

    SpringDataJPA / JPA Auditing

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -3,7 +3,6 @@ package com.sparta.memo.controller;
 import com.sparta.memo.dto.MemoRequestDTO;
 import com.sparta.memo.dto.MemoResponseDTO;
 import com.sparta.memo.service.MemoService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
